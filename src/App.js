18b4b107341898a44f7e5e63ce22b981789c5475
Madhura Mhatre <90045335+madorwhat@users.noreply.github.com>
2022-08-27 13:44:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Fuctionalcmp from './components/Fuctionalcmp'
+import Classcmp from './components/Classcmp'
+
+function App() {
+  return (
+    <div>
+      <Classcmp></Classcmp>
+      
+      
+    </div>
+  );
+}
+
+export default App;
